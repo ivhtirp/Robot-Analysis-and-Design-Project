d321666d574be527b958bab466d8fe7e0bfbee62
Prithvi <deshpandeprithvi@gmail.com>
2019-10-06 18:23:37 +0530

    GUI Added
    GUI added. Needs exception handling and a button to clear array of DH parameters

--- src/Beta.py
+++ src/Beta.py
@@ -1,3 +1,6 @@
+from tkinter import *
+from tkinter import ttk
+import copy
 from mpl_toolkits import mplot3d
 from mpl_toolkits.mplot3d import Axes3D
 import numpy as np
@@ -7,6 +10,9 @@ import math
 import copy
 from dataclasses import dataclass
 
+def setDOF():
+  dof=int(variable1.get())
+  return ''
 class point:
     xPos:float
     yPos:float
@@ -16,7 +22,6 @@ class point:
         self.yPos=y
         self.zPos=z
 
-##WORKING#########################################################
 def zTranslation(transZ):
     zTransMatrix= np.array([[1,0,0,0],[0,1,0,0],[0,0,1,transZ],[0,0,0,1]])
     return zTransMatrix
@@ -47,47 +52,80 @@ def drawGraph(*plotPoints):
         zPoints.append(i.zPos)
     print(xPoints,yPoints,zPoints)
     ax.scatter(xPoints,yPoints,zPoints,color='r',marker='x')
-    ax.plot(xPoints, yPoints, zPoints, color = 'b')
+    ax.plot(xPoints, yPoints, zPoints, color = 'b',linewidth=2)
     matplotlib.pyplot.show()
-dhTable= np.array([[0,10,0,0],[0,0,20,0],
-                   [0,10,0,0],[0,0,20,0]])
-origin= point(0,0,0)
-newPoint= point(0,0,0)
-plotPoints=[]
-plotPoints.append(origin)
-a= np.array([[1,0,0,0],[0,1,0,0],[0,0,1,0],[0,0,0,1]])
-for i in range(0,4):
-
-    j=0
-
-    while j<3:
-        if j==0:
-
-            print(zRotation(math.radians(dhTable[i][j])))
-            a=np.dot(a,zRotation(math.radians(dhTable[i][j])))
-            j=j+1
-
-        if j==1:
-            print(zTranslation(dhTable[i][j]))
-            a=np.dot(a,zTranslation(dhTable[i][j]))
-            j=j+1
-
-        if j==2:
-            print(xTranslation(dhTable[i][j]))
-            a=np.dot(a  ,xTranslation(dhTable[i][j]))
-            j=j+1
-
-        if j==3:
-            print(xRotation(math.radians(dhTable[i][j])))
-            a=np.dot(a,xRotation(math.radians(dhTable[i][j])))
-            j=j+1
-
-        newPoint.xPos= a[0][3]
-        newPoint.yPos= a[1][3]
-        newPoint.zPos= a[2][3]
-        appendPoint= copy.deepcopy(newPoint)
-        plotPoints.append(appendPoint)
-        print("NEWPOINT", newPoint.xPos,newPoint.yPos,newPoint.zPos,"NEWPOINT")
 
 
-drawGraph(*plotPoints)
+def calc(*dhParams):
+    dof=int(len(dhParams)/4)
+    dhTable= np.reshape(dhParams,(dof,4))
+    origin= point(0,0,0)
+    newPoint= point(0,0,0)
+    plotPoints=[]
+    plotPoints.append(origin)
+    a= np.array([[1,0,0,0],[0,1,0,0],[0,0,1,0],[0,0,0,1]])
+    for i in range(0,dof):
+
+        j=0
+
+        while j<3:
+            if j==0:
+                a=np.dot(a,zRotation(math.radians(dhTable[i][j])))
+                j=j+1
+
+            if j==1:
+                a=np.dot(a,zTranslation(dhTable[i][j]))
+                j=j+1
+
+            if j==2:
+                a=np.dot(a  ,xTranslation(dhTable[i][j]))
+                j=j+1
+
+            if j==3:
+                a=np.dot(a,xRotation(math.radians(dhTable[i][j])))
+                j=j+1
+
+            newPoint.xPos= a[0][3]
+            newPoint.yPos= a[1][3]
+            newPoint.zPos= a[2][3]
+            appendPoint= copy.deepcopy(newPoint)
+            plotPoints.append(appendPoint)
+
+
+
+    drawGraph(*plotPoints)
+
+def inputDH(dof):
+    dhParams=[]
+    rows = []
+    Label(text="\u03F4").grid(row=1,column=0)
+    Label(text="z").grid(row=1,column=1)
+    Label(text="x").grid(row=1,column=2)
+    Label(text="\u03B1").grid(row=1,column=3)
+    for i in range(dof):
+        cols = []
+        for j in range(4):
+            e = Entry(relief=RIDGE)
+            e.grid(row=i+2, column=j, sticky=NSEW)
+            e.insert(END, '%d' % (0))
+            cols.append(e)
+        rows.append(cols)
+
+    def onPress():
+        for row in rows:
+            for col in row:
+                dhParams.append(int(copy.deepcopy(col.get())))
+        calc(*dhParams)
+
+    Button(text='Plot', command=onPress).grid()
+    root1.mainloop()
+
+root1=Tk()
+variable1=StringVar()
+def setDOF():
+  dof=int(copy.deepcopy(variable1.get()))
+  inputDH(dof)
+ttk.Entry(width=7, textvariable=variable1).grid(column=1, row=0)
+ttk.Label(text="Enter DOF:").grid(column=0, row=0)
+ttk.Button(text="Set", command=setDOF).grid(column=2, row=0)
+root1.mainloop()
